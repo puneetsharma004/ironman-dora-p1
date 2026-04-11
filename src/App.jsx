174919e526295539269d88b6be09315411206240
Puneet <checkout206@gmail.com>
2026-04-11 17:01:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Hero} from "./Sections/Hero.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,9 @@
+export const Hero = () => {
+    return (
+        <>
+            <seciton classname="bg-red-500 h-screen text-white home">
+                <h1 className="text-4xl">IRONMAN</h1>
+            </seciton>
+        </>
+    )
+}
